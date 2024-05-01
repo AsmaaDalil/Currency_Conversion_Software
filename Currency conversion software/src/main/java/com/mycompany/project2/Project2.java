c39436287a035374058fcab7db63463f16d49be7
Asmaa Dalil <108135622+AsmaaDalil@users.noreply.github.com>
2024-05-01 19:00:20 +0300

    Add files via upload

---
+++ Currency conversion software/src/main/java/com/mycompany/project2/Project2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project2;
+
+/**
+ *
+ * @author ACAD CENTER
+ */
+public class Project2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
